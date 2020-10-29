d5443bbeee649e1e7256dbbb550ff29f6bc640dc
Alice Golter <inkflight@gmail.com>
2020-10-29 18:08:29 -0500

    added the initial coding HTML etc.

---
+++ script.js
@@ -0,0 +1,22 @@
+function main () {
+
+    // Step One: check local storage for old searches
+    
+    // A: there ARE previous searches
+
+    // B: there are NOT previous searches
+        // fetch city weather data
+
+    // Step Two: 
+    
+    
+
+};
+
+
+$(document).ready(function() {
+// when the document has loaded, perform the following function...
+
+    main();
+    // this is the main function, so when the document is fully loaded do this one thing
+})
